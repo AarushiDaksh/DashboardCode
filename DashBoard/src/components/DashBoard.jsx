729faea35add6a62dff78d445b0e79bce5b87f1b
Aarushi Daksh <136158651+AarushiDaksh@users.noreply.github.com>
2025-03-04 22:53:56 +0530

    initial commit

--- DashBoard/src/components/DashBoard.jsx
+++ DashBoard/src/components/DashBoard.jsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
     <main className="flex-1 bg-gray-100 p-8">
       <h1 className="text-4xl font-bold mb-6">CodeTikki</h1>
       
-      {/* Responsive Grid Layout */}
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         <EventCard title="Total Events" count={24} bgColor="bg-orange-500" />
         <EventCard title="Ended Events" count={10} bgColor="bg-gray-700" />
